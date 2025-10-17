be54f0b2573ecfdef7401297145a52281aabf384
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-10-17 16:17:45 -0400

    Create custom tabs bar and add files for all new tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,167 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { HapticTab } from "@/components/haptic-tab";
+import { Colors } from "@/constants/theme";
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import Octicons from "@expo/vector-icons/Octicons";
+import Entypo from "@expo/vector-icons/Entypo";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const TabIcon = (props: any) => {
+  const IconComponent = props.IconComponent;
+  const name = props.name;
+  const size = props.size;
+  const color = props.color;
+  const focused = props.focused;
+
+  return (
+    <View style={styles.tabIconContainer}>
+      <IconComponent name={name} size={size} color={color} />
+      {focused && (
+        <View style={[styles.indicatorDot, { backgroundColor: color }]} />
+      )}
+    </View>
+  );
+};
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
+      safeAreaInsets={{ bottom: 0 }}
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: Colors[colorScheme ?? "light"].tabsColor,
+          position: "absolute",
+          bottom: 35,
+          borderRadius: 25,
+          marginHorizontal: 20,
+          height: 60,
+          paddingBottom: 0,
+        },
+        tabBarItemStyle: {
+          padding: 0,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarIconStyle: {
+          marginVertical: 0,
+          paddingBottom: 2,
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Home",
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              IconComponent={Octicons}
+              name="home"
+              size={24}
+              color={color}
+              focused={focused}
+            />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="reward"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Rewards",
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              IconComponent={Octicons}
+              name="trophy"
+              size={24}
+              color={color}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="transaction"
+        options={{
+          title: "Transaction",
+          tabBarIcon: ({ focused }) => (
+            <View style={styles.transactionBtn}>
+              <Octicons
+                name="feed-plus"
+                size={40}
+                color={
+                  focused
+                    ? Colors[colorScheme ?? "light"].tabIconSelected
+                    : Colors[colorScheme ?? "light"].secondary1
+                }
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="budget"
+        options={{
+          title: "Budget",
+          tabBarIcon: ({ focused, color }) => (
+            <TabIcon
+              IconComponent={Entypo}
+              name="circular-graph"
+              size={24}
+              color={color}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              IconComponent={Octicons}
+              name="person"
+              size={24}
+              color={color}
+              focused={focused}
+            />
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  transactionBtn: {
+    width: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    paddingBottom: 0,
+  },
+  tabIconContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%",
+    paddingBottom: 5,
+  },
+  indicatorDot: {
+    position: "absolute",
+    bottom: 5,
+    width: 5,
+    height: 5,
+    borderRadius: 3,
+  },
+});
--- constants/theme.ts
+++ constants/theme.ts
@@ -3,51 +3,56 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-import { Platform } from 'react-native';
+import { Platform } from "react-native";
 
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
+const tintColorLight = "#DA9DF7";
+const tintColorDark = "#8C17C4";
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    text: "#2B2B2B",
+    background: "#E8E8E1",
     tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
+    icon: "#687076",
+    tabIconDefault: "#E8E8E1",
     tabIconSelected: tintColorLight,
+    tabsColor: "#4C4C4C",
+    secondary1: "#F9A5A5",
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    text: "#ECEDEE",
+    background: "#3A3A39",
     tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
+    icon: "#9BA1A6",
+    tabIconDefault: "#3A3A39",
     tabIconSelected: tintColorDark,
+    tabsColor: "#B4B4B4",
+    secondary1: "#B73684",
   },
 };
 
 export const Fonts = Platform.select({
   ios: {
     /** iOS `UIFontDescriptorSystemDesignDefault` */
-    sans: 'system-ui',
+    sans: "system-ui",
     /** iOS `UIFontDescriptorSystemDesignSerif` */
-    serif: 'ui-serif',
+    serif: "ui-serif",
     /** iOS `UIFontDescriptorSystemDesignRounded` */
-    rounded: 'ui-rounded',
+    rounded: "ui-rounded",
     /** iOS `UIFontDescriptorSystemDesignMonospaced` */
-    mono: 'ui-monospace',
+    mono: "ui-monospace",
   },
   default: {
-    sans: 'normal',
-    serif: 'serif',
-    rounded: 'normal',
-    mono: 'monospace',
+    sans: "normal",
+    serif: "serif",
+    rounded: "normal",
+    mono: "monospace",
   },
   web: {
     sans: "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif",
     serif: "Georgia, 'Times New Roman', serif",
-    rounded: "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
+    rounded:
+      "'SF Pro Rounded', 'Hiragino Maru Gothic ProN', Meiryo, 'MS PGothic', sans-serif",
     mono: "SFMono-Regular, Menlo, Monaco, Consolas, 'Liberation Mono', 'Courier New', monospace",
   },
 });
